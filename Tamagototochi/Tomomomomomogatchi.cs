279eec91296eec49c1995a95f98be1d80b351fc9
Julius Jansson <julius.jansson@elev.ga.ntig.se>
2024-09-10 11:13:59 +0200

    haewfjiohf

--- Tamagototochi/Tomomomomomogatchi.cs
+++ Tamagototochi/Tomomomomomogatchi.cs
@@ -1,5 +1,24 @@
 
 public class Tomomomomomogatchi
 {
+private int hunger;
+private int boredom;
+private List<string> words;
+private bool isAlive;
+private Random generator;
+public string name;
+
+
+public void Feed(){
+
+}
+
+public void Hi(){
+
+}
+
+public void
+public void
+public void
 
 }
